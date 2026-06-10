0dfd118f76976dfbc1ab1cc4a6d730955b619270
bakscratch <lt_8@hotmail.co.uk>
2026-06-10 18:10:46 +0100

    Fix vertex paint not having any lighting causing black square (#5047)
    https://files.facepunch.com/louie/1b0911b1/sbox-dev_16jRRJRsjP.png

--- game/addons/tools/Code/Scene/Mesh/Tools/VertexPaintTool.UI.cs
+++ game/addons/tools/Code/Scene/Mesh/Tools/VertexPaintTool.UI.cs
@@ -312,13 +312,18 @@ partial class VertexPaintTool
 				World = world
 			};
 
-			var light = new ScenePointLight( world )
+			var light = new SceneSpotLight( world )
 			{
 				Radius = 4000,
-				LightColor = Color.White * 0.8f,
+				LightColor = Color.White * 0.7f,
 				Position = new Vector3( 0, 0, 100 ),
+				ConeOuter = 89,
+				ConeInner = 75,
+				QuadraticAttenuation = 5f,
 				ShadowsEnabled = true
 			};
+
+			light.Rotation = Rotation.From( 90, 0, 0 );
 
 			var mesh = CreatePlane( new Color( mask.x, mask.y, mask.z, mask.w ) );
 			var model = Model.Builder
